f891aceec56e11b31c5886a855d6e5a1ffc316a9
rmn0x01 <mnizar9292@gmail.com>
2026-06-16 15:46:08 +0700

    add AI anaylze weekly

--- src/routers/_public.js
+++ src/routers/_public.js
@@ -5,6 +5,7 @@ const HomeController = require('../controllers/home')
 const AuthController = require('../controllers/auth')
 
 router.get('/', HomeController.getHome)
+router.get('/debug', HomeController.doDebug)
 router.post('/login', AuthController.login)
 
 module.exports = router
--- src/services/home.js
+++ src/services/home.js
@@ -1,6 +1,26 @@
+const http = require('http')
+
 const HomeService = {
   async printHome() {
     return 'home'
+  },
+
+  async debugOllamaTags() {
+    return new Promise((resolve, reject) => {
+      http.get('http://localhost:11434/api/tags', (res) => {
+        let data = ''
+        res.on('data', (chunk) => { data += chunk })
+        res.on('end', () => {
+          try {
+            resolve(JSON.parse(data))
+          } catch {
+            resolve(data)
+          }
+        })
+      }).on('error', (err) => {
+        reject(err)
+      })
+    })
   }
 }
 
